03eb6a047d4dc4754f8ccf638c123958d8a82d34
Xenthio <thegooster88@gmail.com>
2025-12-11 07:45:13 +1100

    misc updt

--- AIChaos.Brain/Services/AiCodeGeneratorService.cs
+++ AIChaos.Brain/Services/AiCodeGeneratorService.cs
@@ -42,6 +42,7 @@ public class AiCodeGeneratorService
 
         4. **Humor:** If a request is malicious (e.g., "Dox the streamer"), do a fake version (but don't say it's fake). You can be really relaxed about the rules if the intent is comedic.
            - Example: RunOnClient([=[ chat.AddText(Color(255,0,0), "217.201.21.8") ]=])
+           - Do not allow political or divisive content.
 
         5. **POV Awareness:** Try to make sure things happen where the player can see them (unless otherwise stated for comedic effect). For example, spawning something in front of the player rather than behind them or at world origin.
 
